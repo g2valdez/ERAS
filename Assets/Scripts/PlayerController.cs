267fbce1bc1feebdc54036592ddc395615b92261
Guillermo Valdez <guillermo.valdez@everyonecounts.com>
2018-01-15 01:32:06 -0800

    simple ERA behavior working properly

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	private Vector3 absolutePosition = new Vector3(0,0,0);
+	public Vector3[] displacements = new Vector3[3];
+	private int era = 1;
+	private float xBound = 2.5f;
+	private float yBound = 1.3f;
+	// Use this for initialization
+	void Start () {
+		displacements[0] = new Vector3(-5.9f, 0.0f, 0.0f);
+		displacements[1] = new Vector3(0.0f, 0.0f, 0.0f);
+		displacements[2] = new Vector3(5.9f, 0.0f, 0.0f);
+	}
+	
+	// Update is called once per frame
+	void Update()
+    {
+    	if (Input.GetKeyDown(KeyCode.Alpha1)) era = 0;
+    	if (Input.GetKeyDown(KeyCode.Alpha2)) era = 1;
+    	if (Input.GetKeyDown(KeyCode.Alpha3)) era = 2;
+
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 3.0f;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+        absolutePosition += new Vector3(x, z, 0);
+
+        //check boundaries
+        // print(absolutePosition);
+        if (absolutePosition.x > xBound) absolutePosition.x = xBound;
+        if (absolutePosition.x < -xBound) absolutePosition.x = -xBound;
+        if (absolutePosition.y > 4.6f) absolutePosition.y = 4.6f;
+        if (absolutePosition.y < -yBound) absolutePosition.y = -yBound;
+
+        transform.position = absolutePosition + displacements[era];
+
+    }
+}
---
+++ Assets/Scripts/gamestate.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class gamestate : MonoBehaviour {
+
+	public Tilemap tilemap;
+	public Tile[] tileAssets = new Tile[3];
+
+	private AbsoluteTile[,] absTileStage = new AbsoluteTile[7,8];
+
+
+	// Use this for initialization
+	void Start () {
+
+		for (int j = 0; j < 7; j++) {
+			for (int k = 0; k < 8; k++) {
+				absTileStage[j,k] = new AbsoluteTile(tileAssets);
+			}
+		}
+
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 7; j++) {
+				for (int k = 0; k < 8; k++) {
+					tilemap.SetTile(new Vector3Int(j-11+(i*7),k-2,0), absTileStage[j,k].tileAssets[i]);
+				}
+			}
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
