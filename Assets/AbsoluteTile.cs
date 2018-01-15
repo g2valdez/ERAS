1b9091a39162983e1b582fbf6ecce004a32dce25
Guillermo Valdez <guillermo.valdez@everyonecounts.com>
2018-01-14 22:22:56 -0800

    eras are cool

---
+++ Assets/AbsoluteTile.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+// public class AbsoluteTile : MonoBehaviour {
+// 	public Tile birthTile;
+// 	public Tile lifeTile;
+// 	public Tile deathTile;
+
+// 	// Use this for initialization
+// 	void Start () {
+
+// 	}
+	
+// 	// Update is called once per frame
+// 	void Update () {
+		
+// 	}
+// }
+
+public class AbsoluteTile {
+	public Tile[] tileAssets;
+
+	// Use this for initialization
+	public AbsoluteTile(Tile[] theTiles) {
+		tileAssets = theTiles;
+	} 
+
+}
--- Assets/gamestate.cs
+++ Assets/gamestate.cs
@@ -6,15 +6,32 @@ using UnityEngine.Tilemaps;
 public class gamestate : MonoBehaviour {
 
 	public Tilemap tilemap;
-	public Tile defaultTile;
+	public Tile[] tileAssets = new Tile[3];
+
+	private AbsoluteTile[,] absTileStage = new AbsoluteTile[6,6];
+
 
 	// Use this for initialization
 	void Start () {
-		// tilemap.BoxFill(Vector3Int.zero,defaultTile, 0, 0, 100, 100);
-		tilemap.SetTile(new Vector3Int(0,4,0), defaultTile);
-		tilemap.SetTile(new Vector3Int(8,0,0), defaultTile);
-		tilemap.SetTile(new Vector3Int(0,-5,0), defaultTile);
-		tilemap.SetTile(new Vector3Int(-9,0,0), defaultTile);
+
+		for (int j = 0; j < 6; j++) {
+			for (int k = 0; k < 6; k++) {
+				absTileStage[j,k] = new AbsoluteTile(tileAssets);
+			}
+		}
+
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 6; j++) {
+				for (int k = 0; k < 6; k++) {
+					tilemap.SetTile(new Vector3Int(j-9+(i*6),k-1,0), absTileStage[j,k].tileAssets[i]);
+				}
+			}
+		}
+
+		tilemap.SetTile(new Vector3Int(0,4,0), tileAssets[1]);
+		tilemap.SetTile(new Vector3Int(8,0,0), tileAssets[1]);
+		tilemap.SetTile(new Vector3Int(0,-5,0), tileAssets[1]);
+		tilemap.SetTile(new Vector3Int(-9,0,0), tileAssets[1]);
 
 	}
 	
